48db2ecfc823ec60effda0e8344b26938c85995d
karyssa-dandrea <karyssadandrea@gmail.com>
2021-10-11 16:19:45 -0700

    added my category and description along with some more css

--- crystals.js
+++ crystals.js
@@ -3,31 +3,42 @@ export const crystals = [
         id: '1',
         name: 'Onyx',
         img: './assets/onyx.jpeg',
-        price: 40.00
+        price: 40.00,
+        category: 'strength',
+        description: 'Promotes vigour, steadfastness and stamina.'
+
     },
     { 
         id: '2',
         name: 'Aquamarine',
         img: './assets/aquamarine.jpeg',
-        price: 30.00
+        price: 30.00,
+        category: 'relaxation',
+        description: 'Associated with trusting and letting go and gives a calm feeling.'
     },
     { 
         id: '3',
         name: 'Citrine',
-        img: './assets/citrine.jpeg',
-        price: 35.00
+        img: './assets/citrine.jpeg.webp',
+        price: 35.00,
+        category: 'prosperity',
+        description: 'Attracts wealth, prosperity and success.'
     },
     { 
         id: '4',
         name: 'Lapis',
         img: './assets/lapis.jpeg',
-        price: 45.00
+        price: 45.00,
+        category: 'healing',
+        description: 'Used to encourage self - awareness, self - confidence, self - knowledge, peace and harmony.'
     },
     { 
         id: '5',
         name: 'Aventurine',
         img: './assets/aventurine.jpeg',
-        price: 35.00
+        price: 35.00,
+        category: 'strength',
+        description: 'Renews one’s optimism for life and pushes us to take action to acquire what we want in this world.'
     },
 ];
 
--- render-crystals.js
+++ render-crystals.js
@@ -8,9 +8,15 @@ crystalHeader.textContent = crystal.name;
 const crystalPrice = document.createElement('h3');
 crystalPrice.textContent = `Pricing: $${crystal.price}`;
 
+const crystalCategory = document.createElement('h4');
+crystalCategory.textContent = crystal.category;
+
+const crystalDescription = document.createElement('p');
+crystalDescription.textContent = crystal.description;
+
 const img = document.createElement('img');
 img.src = crystal.img;
 
-crystalCard.append(crystalHeader, img, crystalPrice);
+crystalCard.append(crystalHeader, img, crystalPrice, crystalCategory, crystalDescription);
 return crystalCard;
 }
