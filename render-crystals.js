85257d2ce0b2d6fe26b1f8da3a31c680e3b51f96
karyssa-dandrea <karyssadandrea@gmail.com>
2021-10-11 14:50:33 -0700

    added my pricing to show up on live server

--- render-crystals.js
+++ render-crystals.js
@@ -5,9 +5,12 @@ crystalCard.classList.add('crystal-card');
 const crystalHeader = document.createElement('h2');
 crystalHeader.textContent = crystal.name;
 
+const crystalPrice = document.createElement('h3');
+crystalPrice.textContent = `Pricing: $${crystal.price}`;
+
 const img = document.createElement('img');
 img.src = crystal.img;
 
-crystalCard.append(crystalHeader, img);
+crystalCard.append(crystalHeader, img, crystalPrice);
 return crystalCard;
 }
